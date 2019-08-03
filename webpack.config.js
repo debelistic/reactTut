7063eb9c168e738ed29512984aa2c084771d6ea2
Victor Awotidebe <victorawotidebe@gmail.com>
2019-08-03 18:00:25 +0100

    lesson 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './main.js',
-    output: {
-        path: path.join(__dirname, '/bundle'),
-        filename: 'index_bundle.js'
-    },
-    devServer: {
-        inline: true,
-        port: 9000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env', '@babel/react']
-                }
+   entry: './main.js',
+   output: {
+      path: path.join(__dirname, '/bundle'),
+      filename: 'index_bundle.js'
+   },
+   devServer: {
+      inline: true,
+      port: 5000
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['@babel/preset-env', '@babel/react']
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        })
-    ]
+         }
+      ]
+   },
+   plugins:[
+      new HtmlWebpackPlugin({
+         template: './index.html'
+      })
+   ]
 }
