fba87fbc8b7de8958d85890b7016a9c88cb50454
Victor Awotidebe <victorawotidebe@gmail.com>
2019-08-05 21:25:27 +0100

    pause todo list

--- todoApp/TodoApp.js
+++ todoApp/TodoApp.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import TodoItem from './components/TodoItem';
+import mockTodo from '../mockdata/mockTodo';
 
+const todoItems = mockTodo.map(todoItem => <TodoItem key={todoItem.id} item={todoItem}/>)
 
 const Todo = () => {
   return(
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todoItems}
     </div>
   )
 }
