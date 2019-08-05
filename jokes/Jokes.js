315362e87b15840922acbb5e458effa6e7ad3fd0
Victor Awotidebe <victorawotidebe@gmail.com>
2019-08-05 16:00:23 +0100

    finish props

--- jokes/Jokes.js
+++ jokes/Jokes.js
@@ -2,35 +2,17 @@ import React from 'react';
 import Joke from './components/Joke';
 import JokeData from '../mockdata/jokeData'
 
+const jokes = JokeData.map((joke) =>
+    <Joke
+    key={joke.id}
+    joke={joke}/>
+);
+
+
 const Jokes = () => {
   return (
     <div>
-       <Joke
-        joke={{
-          punchLine: "I don't know, but the flag is a big plus!"
-        }}
-      />
-      <Joke
-        joke={{
-          question: "What's the best thing about Switzerland?",
-          punchLine: "I don't know, but the flag is a big plus!"
-      }}/>
-      <Joke
-        joke={{
-          question: "Did you hear about the mathematician who's afraid of negative numbers?",
-          punchLine: "He'll stop at nothing to avoid them"
-      }}/>
-      <Joke
-        joke={{
-          question: "Hear about the new restaurant called Karma?",
-          punchLine: "There’s no menu: You get what you deserve."
-      }}/>
-      <Joke
-        joke={{
-          question: "Did you hear about the actor who fell through the floorboards?",
-          punchLine: "He was just going through a stage."
-      }}
-      />
+      {jokes}
     </div>
   )
 }
--- jokes/components/Joke.js
+++ jokes/components/Joke.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
+
+const propVariable = (variable, styles, value) => {
+  return (
+    <h3 style={styles}>{variable} : {value}</h3>
+  )
+}
+
+
+
 const Joke = (props) => {
   return (
     <div>
-      <h3
-        style={{display: !props.joke.question && "none"}}>
-          Question: {props.joke.question}
-      </h3>
-      <h3
-        style={{color: !props.joke.question && "#666"}}>
-          Punchline: {props.joke.punchLine}
-      </h3>
+      {propVariable("Question",
+        {display: !props.joke.question && "none"},
+        props.joke.question)}
+    
+      {propVariable("Answer",
+        {color: !props.joke.question && "#666"},
+        props.joke.punchLine)}
       <hr/>
     </div>
   )
