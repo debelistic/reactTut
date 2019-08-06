5a8b772cc5b7e85cce2651c48a68c087717840d6
Victor Awotidebe <victorawotidebe@gmail.com>
2019-08-06 06:04:32 +0100

    change to class set

--- todoApp/TodoApp.js
+++ todoApp/TodoApp.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TodoItem from './components/TodoItem';
 import mockTodo from '../mockdata/mockTodo';
 
 const todoItems = mockTodo.map(todoItem => <TodoItem key={todoItem.id} item={todoItem}/>)
 
-const Todo = () => {
-  return(
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  )
+class Todo extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todoItems,
+    }
+  }
+
+  render() {
+    return (
+      <div className="todo-list">
+        {this.state.todoItems}
+      </div>
+    )
+  }
 }
 
 export default Todo;
