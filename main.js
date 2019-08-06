78c09bbd9e7ceaf0a98683475f2c7c462daed88d
Victor Awotidebe <victorawotidebe@gmail.com>
2019-08-06 05:19:36 +0100

    class states

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './classComponents/App';
+import App from './states/App';
 import './styles/TodoApp.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
