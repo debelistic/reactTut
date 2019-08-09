5d1fe8971d5845e7f82ca5fc931c61d4dfaabf0c
Victor Awotidebe <victorawotidebe@gmail.com>
2019-08-09 16:38:11 +0100

    add styles on complete

--- todoApp/components/TodoItem.js
+++ todoApp/components/TodoItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
+const completedStyle = {
+  color: "green",
+  textDecorationLine: "line-through",
+  fontFamily: "Roboto"
+}
+
+const incompletedStyle = {
+  color: "red",
+  textDecorationLine: "none",
+  fontFamily: "Roboto"
+}
+
 const TodoItem = (props) => {
   return(
     <div className="todo-item">
       <input type="checkbox"
         checked={props.item.completed}
         onChange={() => props.onChange(props.item.id)}/>
-      <p>{props.item.text}</p>
+      <p style={
+        props.item.completed ? completedStyle : incompletedStyle
+      }>{props.item.text}</p>
     </div>
   )
 }
